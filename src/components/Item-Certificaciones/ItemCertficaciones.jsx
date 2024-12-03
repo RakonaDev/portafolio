a97634dee40d089b3f1d25449610a1e83815f046
RakonaDev <juancajas1905@gmail.com>
2024-12-03 09:23:59 -0500

    Adding animations

--- src/components/Item-Certificaciones/ItemCertficaciones.jsx
+++ src/components/Item-Certificaciones/ItemCertficaciones.jsx
@@ -1,14 +1,47 @@
 import PropTypes from "prop-types";
-import './ItemCertificaciones.css'
+import "./ItemCertificaciones.css";
+import { motion } from "motion/react";
 
-export function ItemCertficaciones({ img, nombre, urlCertificado, descripcion }) {
+export function ItemCertficaciones({
+  img,
+  nombre,
+  urlCertificado,
+  descripcion,
+}) {
   return (
     <li className="item-certificaciones">
       <img src={img} alt="linux" className="logo-certificacion" />
       <div className="my-auto body-certificaciones">
-        <p className="text-light fs-3 fw-bold"> {nombre} </p>
-        <p className="text-warning">{descripcion}</p>
-        <a href={urlCertificado} className="btn-certificado" target="_blank">Mostrar Certificado</a> 
+        <motion.p
+          initial={{ opacity: 0, transform: "translateX(100px)" }}
+          whileInView={{ opacity: 1, transform: "translateX(0)" }}
+          transition={{ duration: 0.7 }}
+          viewport={{ once: false }}
+          className="text-light fs-3 fw-bold"
+        >
+          {nombre}
+        </motion.p>
+
+        <motion.p
+          initial={{ opacity: 0, transform: "translateX(100px)" }}
+          whileInView={{ opacity: 1, transform: "translateX(0)" }}
+          transition={{ duration: 0.7 }}
+          viewport={{ once: false }}
+          className="text-warning"
+        >
+          {descripcion}
+        </motion.p>
+
+        <motion.div 
+          initial={{ opacity: 0, transform: "translateY(50px)" }}
+          whileInView={{ opacity: 1, transform: "translateY(0)" }}
+          transition={{ duration: 0.7 }}
+          viewport={{ once: false }}
+        >
+          <a href={urlCertificado} className="btn-certificado" target="_blank">
+            Mostrar Certificado
+          </a>
+        </motion.div>
       </div>
     </li>
   );
